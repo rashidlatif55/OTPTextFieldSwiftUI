747be95146744e3475bfe95c6ccec9beeb1a8b1e
Rashid Latif <rashid@vaultspay.ae>
2024-07-26 15:43:09 +0400

    Video demo

--- OTPTextFieldSwiftUI/ContentView.swift
+++ OTPTextFieldSwiftUI/ContentView.swift
@@ -6,81 +6,39 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @State private var text: String = ""
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+        NavigationStack {
+            VStack {
+                OTPTextFieldView(text: $text) { enteredOtpText in
+                    print(enteredOtpText)
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+                
+                Button {
+                    // Verification logic
+                } label: {
+                    Text("Verify")
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .padding(.vertical, 12)
+                        .frame(maxWidth: .infinity)
+                        .background(
+                            RoundedRectangle(cornerRadius: 16, style: .continuous).fill(.blue)
+                        )
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
+                .disableWithOpacity(condition: text.count < 6)
             }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            .padding(.horizontal, 15)
+            .navigationTitle("Verify OTP")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 #Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    ContentView()
 }
--- OTPTextFieldSwiftUI/OTPTextFieldView.swift
+++ OTPTextFieldSwiftUI/OTPTextFieldView.swift
@@ -8,11 +8,85 @@
 import SwiftUI
 
 struct OTPTextFieldView: View {
+    
+    @Binding var text: String
+    @State var count:Int = 6
+    @FocusState var isTextFieldFocused:Bool
+    var onComplete: ((_ enteredOtpText:String) -> Void)?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 0) {
+            ForEach(0..<count, id: \.self){ index in
+                OTPTextBox(index: index)
+            }
+        }
+        .background {
+            TextField("", text: self.$text)
+                .keyboardType(.numberPad)
+                .textContentType(.oneTimeCode)
+            //Hide textField
+                .frame(width: 1, height: 1)
+                .opacity(0.001)
+                .blendMode(.screen)
+                .focused($isTextFieldFocused)
+                .onChange(of: text) { oldValue , newValue in
+                    if newValue.count > self.count { // Set your max length here4
+                        text = String(newValue.prefix(self.count))
+                    }
+                    
+                    if newValue.count == count {
+                        onComplete?(newValue)
+                    }
+                }
+            
+            
+        }
+        .contentShape(Rectangle())
+        
+        .onTapGesture {
+            self.isTextFieldFocused.toggle()
+        }
+        .padding(.bottom, 20)
+        .padding(.top, 10)
+        
+    }
+    
+    
+    @ViewBuilder
+    func OTPTextBox(index:Int) -> some View {
+        ZStack {
+            if text.count > index {
+                let startIndex = self.text.startIndex
+                let characterIndex = self.text.index(startIndex, offsetBy: index)
+                let charToString = String(self.text[characterIndex])
+                Text(charToString)
+                
+            }else {
+                Text("")
+            }
+        }
+        .frame(minWidth: 44, minHeight: 44)
+        .background {
+            let status = (self.isTextFieldFocused && self.text.count == index)
+            RoundedRectangle(cornerRadius: 6, style: .continuous)
+                .stroke(
+                    status ? .black : .gray,
+                    lineWidth: status ? 1 : 0.5
+                )
+                .animation(.easeInOut(duration: 0.2), value: status)
+        }
+        .frame(maxWidth: .infinity)
     }
 }
 
 #Preview {
-    OTPTextFieldView()
+    ContentView()
+}
+
+extension View {
+    func disableWithOpacity(condition:Bool) -> some View {
+        self
+            .disabled(condition)
+            .opacity(condition ? 0.65 : 1)
+    }
 }
