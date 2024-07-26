6ca18da12856ee9c29edd3016bf6a7971d9c7cd5
Rashid Latif <rashid@vaultspay.ae>
2024-07-26 15:37:00 +0400

    first commit

---
+++ OTPTextFieldSwiftUI/OTPTextFieldView.swift
@@ -0,0 +1,18 @@
+//
+//  OTPTextFieldView.swift
+//  OTPTextFieldSwiftUI
+//
+//  Created by Rashid Latif on 26/07/2024.
+//
+
+import SwiftUI
+
+struct OTPTextFieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OTPTextFieldView()
+}
